634d7f1c9f46d085a8fec6a697b040ec78641be8
Kevin Le <kevinle108@gmail.com>
2021-04-15 00:31:43 -0400

    try in progress

--- Program.cs
+++ Program.cs
@@ -8,69 +8,54 @@ namespace Day19
     {
         static void Main(string[] args)
         {
-            //string data = File.ReadAllText("nasa2.json");
-
-            //using var doc = JsonDocument.Parse(data);
-            //JsonElement root = doc.RootElement;
-
-            //Console.WriteLine("The root is of type " + root.ValueKind);
 
             //EnumerateDoc("nasa2.json");
             EnumerateDoc("countries.json");
 
-        }
 
+        }        
         static void EnumerateDoc(string fileName)
         {
-            string data = File.ReadAllText(fileName);
-            JsonDocument doc = JsonDocument.Parse(data);
-            EnumerateElement(doc.RootElement, "");
+            using JsonDocument doc = JsonDocument.Parse(File.ReadAllText(fileName));
+            JsonElement root = doc.RootElement;
+            EnumerateEle(root, "");
         }
-
-        static void EnumerateElement(JsonElement elem, string indentation)
+        static void EnumerateEle(JsonElement ele, string indentation)
         {
-            switch (elem.ValueKind)
+            switch (ele.ValueKind)
             {
-                case JsonValueKind.Array:
-                    for (int i = 0; i < elem.GetArrayLength(); i++)
-                    {
-                        Console.WriteLine(indentation + "Item " + (i + 1) + " of " + elem.GetArrayLength());
-                        EnumerateElement(elem[i], indentation + "    ");
-                        Console.WriteLine();
-                    }
-                    break;
-
+                
                 case JsonValueKind.Object:
-                    var objEnum = elem.EnumerateObject();
+                    Console.WriteLine(indentation + "This is an Object");
+                    JsonElement.ObjectEnumerator objEnum = ele.EnumerateObject();
                     while (objEnum.MoveNext())
                     {
                         JsonProperty prop = objEnum.Current;
-                        string name = prop.Name;
-                        JsonElement value = prop.Value;
-                        switch (value.ValueKind)
-                        {
-                            case JsonValueKind.Array:
-                                Console.WriteLine(indentation + name + " is an array of length " + value.GetArrayLength());
-                                EnumerateElement(value, indentation + "    ");
-                                break;
-
-                            case JsonValueKind.Object:
-                                Console.WriteLine(indentation + name + " is an object");
-                                EnumerateElement(value, indentation + "    ");
-                                break;
-
-                            default:
-                                Console.WriteLine(indentation + name + ": " + value);
-                                break;
-                        }
+                        string propName = prop.Name;
+                        JsonElement propValue = prop.Value;
+                        Console.WriteLine(indentation + " " + $"{propName} is an {propValue.ValueKind}");
+                        EnumerateEle(propValue, indentation + "  ");
                     }
                     break;
+                case JsonValueKind.Array:
+                    Console.WriteLine(indentation + $"This is an Array of {ele.GetArrayLength()} items:");
 
+                    JsonElement.ArrayEnumerator arrEnum = ele.EnumerateArray();
+                    int i = 1;
+                    
+                    while (arrEnum.MoveNext())
+                    {                        
+                        Console.WriteLine(indentation + $"Item {i} of {ele.GetArrayLength()}");
+                        EnumerateEle(arrEnum.Current, indentation + "  ");
+                        Console.WriteLine();
+                        i++;
+                    }
+                    break;               
                 default:
-                    Console.WriteLine(indentation + elem);
+                    Console.WriteLine(indentation + "This is a Primitive");
+                    Console.WriteLine(indentation + ele);
                     break;
             }
         }
-
     }
 }
